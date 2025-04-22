b3ccb7766a3b74f906afc27a699d0058fd204012
Thyago Thayllan Mendes de Sousa <thayllanst@outlook.com>
2025-04-22 00:16:02 -0300

    feat: Criar view de cadastro de Nova Transação

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
+from .views import NewTransaction
 from .views import Transactions
 
 
 app_name = 'transactions'
 
 urlpatterns = [
-    path('', Transactions.as_view(), name='transactions')
+    path('', Transactions.as_view(), name='transactions'),
+    path('cadastrar/', NewTransaction.as_view(), name='register'),
 ]
--- transactions/views.py
+++ transactions/views.py
@@ -1,7 +1,23 @@
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.http import HttpRequest
 from django.http.response import HttpResponse
+from django.shortcuts import redirect
 from django.shortcuts import render
+from django.views import View
 from django.views.generic import TemplateView
+
+
+class NewTransaction(View):
+    def post(self, request: HttpRequest) -> HttpResponse | HttpResponseRedirect:
+        form = TransactionForm(data=request.POST)
+
+        if not form.is_valid():
+            return render(request, 'transactions/transactions.html', {'form': form})
+
+        Transaction.objects.create(**form.cleaned_data, user=request.user)
+
+        return redirect('transactions:transactions')
 
 
 class Transactions(TemplateView):
