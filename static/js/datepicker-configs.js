4aff31da03a8f7d6c504f1efc7b343fcd5a9948c
Thyago Thayllan Mendes de Sousa <thayllanst@outlook.com>
2025-05-03 20:42:07 -0300

    refactor: Criar arquivo para configurações de datepickers

---
+++ static/js/datepicker-configs.js
@@ -0,0 +1,9 @@
+const datepickerByMonth = {
+    autoclose: true,
+    format: "mm/yyyy",
+    language: "pt-BR",
+    maxViewMode: 2,
+    minViewMode: 1,
+    startView: 1,
+    todayBtn: "linked",
+};
