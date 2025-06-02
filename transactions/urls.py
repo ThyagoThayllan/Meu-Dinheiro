22e23f5cc79fa1187752f06437b5900470baf693
Thyago Thayllan Mendes de Sousa <thyago.sousa@vcx.solutions>
2025-06-02 00:12:09 -0300

    feat: Criar URL para view TransactionsAjax.

--- transactions/urls.py
+++ transactions/urls.py
@@ -4,6 +4,7 @@ from .views import DeleteTransaction
 from .views import EditTransaction
 from .views import NewTransaction
 from .views import Transactions
+from .views import TransactionsAjax
 
 
 app_name = 'transactions'
@@ -13,4 +14,5 @@ urlpatterns = [
     path('cadastrar/', NewTransaction.as_view(), name='register'),
     path('<int:pk>/editar/', EditTransaction.as_view(), name='edit'),
     path('<int:pk>/deletar/', DeleteTransaction.as_view(), name='delete'),
+    path('ajax/<str:period>/', TransactionsAjax.as_view(), name='transactions_ajax'),
 ]
