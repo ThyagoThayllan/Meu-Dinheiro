f01b15d5c50c7f9da6beeaf8310775df2cb45993
Thyago Thayllan Mendes de Sousa <thyago.sousa@vcx.solutions>
2025-06-02 00:09:58 -0300

    feat: Criar configuração para seletor de perído no formato "MM-YYYY"

--- static/js/datepicker-configs.js
+++ static/js/datepicker-configs.js
@@ -15,3 +15,13 @@ const datepickerByMonth = {
     startView: 1,
     todayBtn: "linked",
 };
+
+const datepickerByPeriod = {
+    autoclose: true,
+    format: "mm-yyyy",
+    language: "pt-BR",
+    maxViewMode: 2,
+    minViewMode: 1,
+    startView: 1,
+    todayBtn: "linked",
+};
