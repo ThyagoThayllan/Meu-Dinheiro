9fc4ab88e009140565e68786514387a433d82b3c
Thyago Thayllan Mendes de Sousa <thayllanst@outlook.com>
2025-06-16 22:25:55 -0300

    feat: Criar URLs para deletar e editar Dívida.

--- debts/urls.py
+++ debts/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
+from .views import DebtDelete
+from .views import DebtEdit
 from .views import Debts
 
 
 app_name = 'debts'
 
 urlpatterns = [
-    path('', Debts.as_view(), name='debts')
+    path('', Debts.as_view(), name='debts'),
+    path('<int:pk>/deletar/', DebtDelete.as_view(), name='delete'),
+    path('<int:pk>/editar/', DebtEdit.as_view(), name='edit'),
 ]
