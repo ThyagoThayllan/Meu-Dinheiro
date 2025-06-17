6482a2ba6aa9c79de96a9bd2734b5fe9857bbb5c
Thyago Thayllan Mendes de Sousa <thayllanst@outlook.com>
2025-06-16 22:14:58 -0300

    chore: Importar recursos necessários para a implementação das Views.

--- debts/views.py
+++ debts/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.contrib import messages
 from django.http import HttpRequest
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
+from django.shortcuts import render
+from django.views import View
 from django.views.generic import TemplateView
+
+from .forms import DebtForm
+from .models import Debt
 
 
 class Debts(TemplateView):
