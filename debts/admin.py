91bb8be754f8661f6e342cf1f10e6f289566c3b5
Thyago Thayllan Mendes de Sousa <thayllanst@outlook.com>
2025-06-29 18:33:27 -0300

    feat: Disponibilizar gerenciamento de Dívidas no admin.

--- debts/admin.py
+++ debts/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from debts.models import Debt
+
+
+@admin.register(Debt)
+class DebtAdmin(ModelAdmin):
+    list_display = [
+        'id',
+        'description',
+        'category',
+        'amount',
+        'updated_at',
+        'created_at',
+        'is_paid',
+    ]
+
+    list_filter = ['category', 'updated_at', 'created_at', 'is_paid']
+
+    list_per_page = 25
+
+    ordering = ['-created_at']
+
+    search_fields = [
+        'amount',
+        'category',
+        'description',
+        'financier',
+        'id',
+    ]
