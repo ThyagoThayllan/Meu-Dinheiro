908dba2e99dd460002bfa5ac59c9990be7652be2
Thyago Thayllan Mendes de Sousa <thayllanst@outlook.com>
2025-06-30 14:23:33 -0300

    feat: Disponibilizar gerenciamento de Transações (Transaction) no admin do sistema.

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from transactions.models import Transaction
+
+
+@admin.register(Transaction)
+class TransactionAdmin(ModelAdmin):
+    list_display = [
+        'id',
+        'destination',
+        'amount',
+        'category',
+        'transaction_type',
+        'date',
+        'is_paid',
+        'created_at',
+        'updated_at',
+    ]
+
+    list_filter = ['is_paid', 'category', 'transaction_type', 'date', 'created_at', 'updated_at']
+
+    list_per_page = 25
+
+    ordering = ['-created_at']
+
+    search_fields = [
+        'amount',
+        'destination',
+        'id',
+    ]
